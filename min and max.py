37b8925f5410f84e94f2c4ef9c5e7c791077f2c1
mr-yuvie <142655484+mr-yuvie@users.noreply.github.com>
2024-02-06 19:07:29 +0530

    Delete min and max.py

--- min and max.py
+++
@@ -1,8 +0,0 @@
-A = []
-n = int(input("Enter Total number of values:"))
-for i in range(n):
-    c = int(input("Enter number:"))
-    A.append(c)
-L = len(A)
-print("Min of", A[: L // 2], "is", min(A[: L // 2]))
-print("Max of", A[L // 2 :], "is", max(A[L // 2 :]))
